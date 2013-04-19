bccf157133b04405e2823b09633df44c804310f3
yugangw <yugangw@microsoft.com>
2013-04-19 12:57:00 -0700

    update assemblyinfo.cs with file version

--- XmlTransform/Properties/AssemblyInfo.cs
+++ XmlTransform/Properties/AssemblyInfo.cs
@@ -1,2 +1,10 @@
-﻿
-[assembly: System.Runtime.InteropServices.ComVisible(false)]
+﻿using System.Reflection;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
